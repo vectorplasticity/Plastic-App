925cf83376bd9e889c238adfb69550bd8bb71edb
vectorplasticity <vectorplasticity@gmail.com>
2025-11-10 00:30:41 -0800

    Update app.py

--- plastic_app/app.py
+++ plastic_app/app.py
@@ -255,5 +255,8 @@ def get_json_report():
     
     return jsonify(report_json)
 
-if __name__ == '__main__':
+def main():
     app.run(debug=True, port=5000)
+    
+if __name__ == '__main__':
+    main()
