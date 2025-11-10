33391442992b48bf468d833779b23d33d6e9499c
vectorplasticity <vectorplasticity@gmail.com>
2025-11-10 00:16:01 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='plastic-app',
+
+    # The version of your package
+    version='0.1.0',
+
+    packages=find_packages(),
+
+    include_package_data=True,
+
+    install_requires=requirements,
+
+    entry_points={
+        'console_scripts': [
+            'plastic-app=plastic_app.app:main',
+        ],
+    },
+
+    author="Vectorplasticity",
+    description="Visualize your app files.",
+    url="https://github.com/vectorplasticity/Plastic-App",
+    python_requires='>=3.6',
+)
